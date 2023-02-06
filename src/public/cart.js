a9b46c11bc89071095997149bf3c42e2e319de2b
Anthony Allois <anthonyallois@gmail.com>
2023-02-06 17:42:22 -0300

    feat: Eliminar Prod del carrito

--- src/public/cart.js
+++ src/public/cart.js
@@ -7,9 +7,14 @@ document.addEventListener("DOMContentLoaded", () => {
   let change_qty = document.getElementsByClassName('change_qty');
   for (let i = 0; i < change_qty.length; i++) {
     const button = change_qty[i];
-    button.addEventListener('change', changeQuantity)          
+    button.addEventListener('change', changeQuantity)
   }   
   
+  let btn_eliminar = document.getElementsByClassName('btn_eliminar');
+  for (let i = 0; i < btn_eliminar.length; i++) {
+    const button = btn_eliminar[i];
+    button.addEventListener('click', deleteProduct)
+  }
   /* document.getElementById('cart_quantity_nav').innerText = data.cantidad; */
   /* document.getElementById('cart_quantity_subtitle').innerText = `El carrito contiene XXX producto`;   */
 
@@ -36,6 +41,22 @@ document.addEventListener("DOMContentLoaded", () => {
     //console.log(card_carrito.getElementsByClassName('id_product')[0].innerText);
   }
   
+  async function deleteProduct(e) {
+    let card_carrito = e.target.parentElement.parentElement.parentElement;
+
+    let id_cart = card_carrito.getElementsByClassName('id_cart')[0].innerText.trim();
+    let id_prod = card_carrito.getElementsByClassName('id_product')[0].innerText.trim();
+
+    let result = await fetch(`/api/carrito/${id_cart}/productos/${id_prod}`, { 
+      method: 'DELETE', 
+      headers: {
+      'Content-Type': 'application/json; charset=UTF-8'
+      } 
+    });    
+    let data = await result.json();
+
+    console.log(data);
+  }
 });
 
 /*
